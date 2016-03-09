50a0eb3fecb319813204d092bb3cd1f6fb9a0def
Jamis Charles <jacharles@paypal.com>
2016-03-08 23:04:29 -0800

    Basic use case works.
    Able to convert currency both ways..

--- public/js/components/Conversion.js
+++ public/js/components/Conversion.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import $ from 'jquery';
 
 var FeesTable = React.createClass({
     propTypes: {
@@ -38,16 +39,76 @@ var Conversion = React.createClass({
     getInitialState() {
         return {
             originAmount: '0.00',
-            destinationAmount: '0.00'
+            originCurrency: 'USD',
+            destinationAmount: '0.00',
+            destinationCurrency: 'EUR',
+            conversionRate: 1
         }
     },
-    handleOriginAmountChange(event) {
-        var originAmount = event.target.value;
+    handleCurrencyChange(currentlyEditing, event) {
+        // console.log('arguments', arguments)
+        var obj = {};
+        if (currentlyEditing === 'origin') {
+            obj.originCurrency = event.target.value
+        } else {
+            obj.destinationCurrency = event.target.value
+        }
 
-        this.setState({
-            originAmount: originAmount,
-            destinationAmount: this.props.conversionRate * originAmount
+        this.setState(obj)
+    },
+    handleOriginAmountChange(currentlyEditing, event) {
+        var newValue = event.target.value;
+        var originCurrency = this.state.originCurrency;
+        var destCurrency = this.state.destinationCurrency;
+
+        var that = this;
+
+        // Thoughts: Often it seems simpler to bundle all this up in one call on the server
+        // but maybe you don't have that luxury...
+
+        var payload = {
+            originCurrency: originCurrency,
+            destCurrency: destCurrency
+        }
+
+        // determine whether we're going from origin or dest amount
+        if (currentlyEditing === 'origin') {
+            payload.originAmount = newValue;
+        } else {
+            payload.destAmount = newValue;
+        }
+
+        // ajax call for destination amount
+        // originCurrency, destCurrency, originAmount
+        $.ajax({
+            method: 'GET',
+            url: '/api/conversion',
+            data: payload
         })
+        .done(function(resp) {
+            // make ajax call to get the fee amount..
+            var newState = {
+                conversionRate: resp.xRate
+            }
+
+            // pull new values from response and from event
+            if (currentlyEditing === 'origin') {
+                newState.originAmount = newValue;
+                newState.destinationAmount = resp.destAmount;
+            } else {
+                newState.originAmount = resp.originAmount;
+                newState.destinationAmount = newValue;
+            }
+
+            that.setState(newState)
+
+        });
+
+        //
+        // ajax call for fees
+        // ajax call for total
+
+
     },
     render(){
         var {conversionRate, fee, total} = this.props;
@@ -55,13 +116,13 @@ var Conversion = React.createClass({
         return (
           <div>
             <label>Convert</label>&nbsp;
-            <input onChange={this.handleOriginAmountChange} value={this.state.originAmount} />
-            <select>
+            <input onChange={this.handleOriginAmountChange.bind(null, 'origin')} value={this.state.originAmount} />
+            <select ref="originCurrency" value={this.state.originCurrency} onChange={this.handleCurrencyChange.bind(null, 'origin')}>
                 <option>USD</option>
                 <option>EUR</option>
             </select>
-            to <input value={this.state.destinationAmount} />&nbsp;
-            <select>
+            to <input onChange={this.handleOriginAmountChange.bind(null, 'destination')} value={this.state.destinationAmount} />&nbsp;
+            <select ref="destCurrency" value={this.state.destinationCurrency} onChange={this.handleCurrencyChange.bind(null, 'destination')}>
                 <option>USD</option>
                 <option>EUR</option>
             </select>
@@ -69,7 +130,7 @@ var Conversion = React.createClass({
 
 
             <br/><br/><br/>
-            <FeesTable originAmount={this.state.originAmount} conversionRate={conversionRate} fee={fee} total={total} />
+            <FeesTable originAmount={this.state.originAmount} conversionRate={this.state.conversionRate} fee={fee} total={total} />
           </div>
         )
     }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/conversion', function(req, res, next) {
+  // randomize the exchange rate
+  var max = 100; // ms
+  var min = 0.1;
+  var xRate = Math.floor(Math.random() * (max - min)) + min;
+
+  var originAmount = req.query.originAmount;
+  var originCurrency = req.query.originCurrency;
+  var destAmount = req.query.destAmount;
+  var destCurrency = req.query.destCurrency;
+
+  // if both currencies are the same, exchange rate will be 1.
+  if (originCurrency === destCurrency) {
+    xRate = 1;
+  }
+
+  if (!originAmount) {
+    originAmount = (parseInt(destAmount, 10) * xRate).toFixed(2);
+  } else {
+    destAmount = (parseInt(originAmount, 10) / xRate).toFixed(2);
+  }
+
+
+  // random timeout to simulate api response times
+  setTimeout(function(){
+    res.json({originAmount: originAmount, destAmount: destAmount, destCurrency: destCurrency, xRate: xRate  })
+  }, getRandomResponseTime())
+});
+
+
+function getRandomResponseTime() {
+  var max = 1200; // ms
+  var min = 150;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 module.exports = router;
